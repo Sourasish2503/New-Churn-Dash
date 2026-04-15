f0bfcc0f5275fe9bfc0f0b320616d9de844918f3
Sourasish2503 <sourasishpaul2503@gmail.com>
2026-04-15 12:54:26 +0000

    Your commit message

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -10,7 +10,7 @@ function getAdminApp(): App {
     credential: cert({
       projectId:   process.env.FIREBASE_PROJECT_ID!,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-      privateKey:  process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+      privateKey:  process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
 }
